d80fefd47ec56059c28730627af596d6a0cc43ca
Kenneth <bacunoabnerkenneth@gmail.com>
2024-12-10 20:38:19 +0800

    nag css ako

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,45 @@
 import React from "react";
 import "./Footer.css";
-import footerimage from "../assets/lgu-logo.png"
-import fbimage from "../assets/fb.png"
-import emailimage from "../assets/email.png"
-import phoneimage from "../assets/telephonee.png"
+import footerimage from "../assets/lgu-logo.png";
+import fbimage from "../assets/fb.png";
+import emailimage from "../assets/email.png";
+import phoneimage from "../assets/telephonee.png";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Contact Us:</p>
-      <ul>
-        <li> <span><img src={fbimage}></img></span>Lgu-Daet Expanded Scholarship Program</li>
-        <li> <span><img src={emailimage}></img></span>Email: lgudaetsprogram@gmail.com</li>
-        <li> <span><img src={phoneimage}></img></span>Phone: 524-565-8653</li>
-      </ul>
-      <img src={footerimage} alt="Footer Logo" />
-      <p>All Rights Reserved.</p>
+      <div className="section1">
+        <p>Contact Us:</p>
+        <ul>
+          <li>
+            {" "}
+            <span>
+              <img src={fbimage}></img>
+            </span>
+            Lgu-Daet Expanded Scholarship Program
+          </li>
+          <li>
+            {" "}
+            <span>
+              <img src={emailimage}></img>
+            </span>
+            Email: lgudaetsprogram@gmail.com
+          </li>
+          <li>
+            {" "}
+            <span>
+              <img src={phoneimage}></img>
+            </span>
+            Phone: 524-565-8653
+          </li>
+        </ul>
+      </div>
+      <div className="section2">
+        <img src={footerimage} alt="Footer Logo" />
+      </div>
+      <div className="section3">
+        <p>All Rights Reserved.</p>
+      </div>
     </footer>
   );
 };
--- src/components/StepbyStep.jsx
+++ src/components/StepbyStep.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./StepbyStep.css"
+import "./StepbyStep.css";
 
 const StepbyStep = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
@@ -9,13 +9,26 @@ const StepbyStep = ({ isOpen, onClose }) => {
       <div className="popup-container">
         <h2>Steps to Apply</h2>
         <div className="popup-content">
-          <p><strong>STEP 1</strong></p>
-          <p>If you're <strong>ELIGIBLE</strong> to apply for the scholarship, register only using a valid email address.</p>
-          <p><strong>STEP 2</strong></p>
-          <p>A confirmation will be sent to your email address upon successful registration.</p>
-          <p><strong>STEP 3</strong></p>
           <p>
-            You can now proceed with filling out the application form. These are the needed requirements:
+            <strong>STEP 1</strong>
+          </p>
+          <p>
+            If you're <strong>ELIGIBLE</strong> to apply for the scholarship,
+            register only using a valid email address.
+          </p>
+          <p>
+            <strong>STEP 2</strong>
+          </p>
+          <p>
+            A confirmation will be sent to your email address upon successful
+            registration.
+          </p>
+          <p>
+            <strong>STEP 3</strong>
+          </p>
+          <p>
+            You can now proceed with filling out the application form. These are
+            the needed requirements:
           </p>
           <ul>
             <li>Personal Data</li>
@@ -26,20 +39,28 @@ const StepbyStep = ({ isOpen, onClose }) => {
             <li>1x1 Picture</li>
             <li>
               Annual Income Tax Return of both parents
-              <ul>
+              <ul className="inner-list">
                 <li>* The system accepts .jpeg, .png file type only.</li>
                 <li>* The system accepts multiple files per upload.</li>
-                <li>* You may press the preview button to check files you submitted after uploading it.</li>
+                <li>
+                  * You may press the preview button to check files you
+                  submitted after uploading it.
+                </li>
               </ul>
             </li>
           </ul>
-          <p><strong>STEP 4</strong></p>
           <p>
-            Make sure that you have accomplished all the needed requirements with true information and correct spellings,
-            and uploaded all the required documents before you press <strong>SUBMIT</strong> button.
+            <strong>STEP 4</strong>
+          </p>
+          <p>
+            Make sure that you have accomplished all the needed requirements
+            with true information and correct spellings, and uploaded all the
+            required documents before you press <strong>SUBMIT</strong> button.
           </p>
         </div>
-        <button className="close-button" onClick={onClose}>Close</button>
+        <button className="close-button" onClick={onClose}>
+          Close
+        </button>
       </div>
     </div>
   );
